54ffda06a580e0c93b8c7d85b4df7e32ddd42e88
distantskyh-cloud <232817334+distantskyh-cloud@users.noreply.github.com>
2026-08-14 02:36:56 +0800

    OE 1 finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    alert(
+        "Welcome to My First Website!\n\n" +
+        "Thank you for visiting our webpage.\n\n" +
+        "This webpage was created using HTML, CSS, and JavaScript."
+    );
+}
